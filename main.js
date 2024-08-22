01a69af3f5a5991ea17a69919edc2c89e16186f6
Ana Carolina Nunes <61066690+ana-carolina-nunes@users.noreply.github.com>
2024-08-22 11:22:48 -0300

    Update main.js: realizando requisição http em loop para buscar dados de candidatos

--- main.js
+++ main.js
@@ -1,25 +1,48 @@
 const btnRequest = document.getElementsByClassName('send_request');
 const txtResponse = document.getElementById("result");
+var url;
 
 onclick = function (evento) {
+    let objJsonCandidatos;
+    let objJsonDadosCandidato;
+    url = "https://divulgacandcontas.tse.jus.br/divulga/rest/v1/candidatura/listar/2024/49590/2045202024/13/candidatos";
 
-    var url = "http://divulgacandcontas.tse.jus.br/divulga/rest/v1/candidatura/listar/2020/35157/2030402020/11/candidatos";//Sua URL
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", url);
+    xhr.send();
+    xhr.responseType = "json";
 
-    var xhttp = new XMLHttpRequest();
-    xhttp.open("GET", url, true);
-    
-    xhttp.onreadystatechange = function(){//Função a ser chamada quando a requisição retornar do servidor
-        if ( xhttp.readyState == 4 && xhttp.status == 200 ) {//Verifica se o retorno do servidor deu certo
-            console.log(':)');
-        }else{
-            console.log(':(');            
+    xhr.onload = () => {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            objJsonCandidatos = xhr.response;
+            console.log(objJsonCandidatos.candidatos);
+            console.log(objJsonCandidatos.candidatos.length);
+
+            for (var i = 0; i < objJsonCandidatos.candidatos.length; i++) {
+                url = "https://divulgacandcontas.tse.jus.br/divulga/rest/v1/candidatura/buscar/2024/49590/2045202024/candidato/" + objJsonCandidatos.candidatos[i].id;
+               
+                const xhrCandidato = new XMLHttpRequest();
+                xhrCandidato.open("GET", url);
+                xhrCandidato.send();
+                xhrCandidato.responseType = "json";
+
+                xhrCandidato.onload = () => {
+                    if (xhrCandidato.readyState == 4 && xhrCandidato.status == 200) {
+                        objJsonDadosCandidato = xhrCandidato.response;
+                        console.log(objJsonDadosCandidato.nomeUrna + '-> '+objJsonDadosCandidato.numero + ' cor/raça: '+objJsonDadosCandidato.descricaoCorRaca);
+
+                    } else {
+                        console.log(`Erro na busca de dados de candidatos: ${xhrCandidato.status}`);
+                    }
+                };
+
+            }
+
+        } else {
+            console.log(`Erro na busca de candidatos: ${xhr.status}`);
         }
-       
-         txtResponse.value = JSON.parse(xhttp.responseText);
-         console.log(JSON.parse(xhttp.responseText));
-        
-        //console.log(xhttp.responseText);
-    }
-    
-    xhttp.send();//A execução do script CONTINUARÁ mesmo que a requisição não tenha retornado do servidor
+    };
+}
+
+function getResponseHttpAsync(url) {  
 }
